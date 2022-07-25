e9373c2e324e61fe305f40deff6fd3a4c7aca70c
ZXT <385189207@qq.com>
2022-07-26 00:34:47 +0800

    test: 测试生成
    test
    
    test2
    
    test3
    
    test: 修改md, 看永久连接是否会变
    
    test
    
    22
    
    test3
    
    test

--- deploy.sh
+++ deploy.sh
@@ -1,70 +1,31 @@
-
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
 
-
-push_addr=`git remote get-url --push origin` 
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
-
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
-cd $dist_path
+cd docs/.vuepress/dist
 
+# deploy to github pages
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='[deploy] manual'
+  githubUrl=git@github.com:zxt385189207/ET-homepage.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://zxt385189207:${GITHUB_TOKEN}@github.com/zxt385189207/ET-homepage.git
+  git config --global user.name "小弟"
+  git config --global user.email "385189207@qq.com"
+fi
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git commit -m "${msg}"
+git push -f $githubUrl HEAD:gh-pages # 推送到github gh-pages分支
+
 
 cd -
-rm -rf $dist_path
+rm -rf docs/.vuepress/dist
 
-##!/usr/bin/env sh
-#
-## 确保脚本抛出遇到的错误
-#set -e
-#
-## 生成静态文件
-#npm run build
-#
-## 进入生成的文件夹
-#cd docs/.vuepress/dist
-#
-## deploy to github pages
-## echo 'doc.et-framework.cn' > CNAME
-#
-#if [ -z "$ACCESS_TOKEN" ]; then
-#  msg='deploy'
-#  githubUrl=git@github.com:zxt385189207/ET-homepage.git
-#else
-#  msg='来自github actions的自动部署'
-#  githubUrl=https://zxt385189207:${ACCESS_TOKEN}@github.com/zxt385189207/ET-homepage.git
-#  git config --global user.name "小弟"
-#  git config --global user.email "385189207@qq.com"
-#fi
-#
-#git init
-#git add -A
-#git commit -m "${msg}"
-#git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
-#
-## deploy to coding pages
-## echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-## echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-#
-## if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-##   codingUrl=git@e.coding.net:xgy/xgy.git
-## else
-##   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-## fi
-## git add -A
-## git commit -m "${msg}"
-## git push -f $codingUrl master # 推送到coding
-#
-#cd -
-## rm -rf docs/.vuepress/dist
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -190,7 +190,7 @@ module.exports = {
 
     markdown: {
         // lineNumbers: true,
-        extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+        extractHeaders: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
     },
 
     // 监听文件变化并重新构建
